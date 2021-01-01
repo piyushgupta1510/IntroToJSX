486d45e88530d1f3bdc1503eeed59b921025fdd0
piyushgupta1510 <piyushgupta1510@gmail.com>
2021-01-02 00:24:06 +0100

    first change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>HELLO World!</h1>
+    <h1>HELLO World after commit!</h1>
     <p>This is a paragraph</p>
   </div>,
   document.getElementById("root")
